f6b9d89989a2de807315729fa92e1ed47813d631
TH Lee <lee@ssccs.org>
2026-04-08 18:38:18 +0200

    #18: setup Docker/devcontainer environments

--- .github/workflows/test-verify-devcontainer.sh
+++
@@ -1,7 +0,0 @@
-act -W .github/workflows/verify-devcontainer.yml \
-  --job validate \
-  --bind \
-  --env CI=true \
-  --rm \
-  --container-architecture linux/amd64 \
-  --platform ubuntu-latest=catthehacker/ubuntu:act-24.04
